5dcb539f5437daad7cc46484d199eca27d69eb81
Pema Wangchuk <pemawangchug1986@gmail.com>
2025-03-23 11:49:37 +0600

    implement layout auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,9 @@
-import { isAuthenticated } from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+
 import React from "react";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
-	const isUserAuthenticated = await isAuthenticated()
-	if(!isUserAuthenticated) {
-		redirect("/sign-in")
-	}
 	return (
 		<div className='root-layout'>
 			<nav>
